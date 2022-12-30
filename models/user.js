71c444c7f9229628cc62982dc1adc0e0c85922cb
Mara Olaru <olarumara20@stud.ase.ro>
2022-12-30 15:54:13 +0200

    starting the user page

--- models/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("user", {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-  });
-};
